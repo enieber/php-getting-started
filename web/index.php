f8eaa660da7bb7ba54455d6f8068eea488f0751f
enieber <eniebercunha@gmail.com>
2014-11-27 19:42:49 -0200

    handler

--- web/index.php
+++ web/index.php
@@ -25,6 +25,23 @@ $app->register(new Herrera\Pdo\PdoServiceProvider(),
     'pdo.password' => $dbopts["pass"]
   )
 );
+
+//handler
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
+});
+
 // Our web handlers
 
 $app->get('/', function() use($app) {
