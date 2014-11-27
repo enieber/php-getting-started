a548c92fc41c56a4bfcb90117dd13819e22bde57
enieber <eniebercunha@gmail.com>
2014-11-27 19:40:43 -0200

    CONNECTION

--- web/index.php
+++ web/index.php
@@ -15,6 +15,16 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path' => __DIR__.'/../views',
 ));
 
+//add conection pdo
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+  array(
+    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+    'pdo.port' => $dbopts["port"],
+    'pdo.username' => $dbopts["user"],
+    'pdo.password' => $dbopts["pass"]
+  )
+);
 // Our web handlers
 
 $app->get('/', function() use($app) {
